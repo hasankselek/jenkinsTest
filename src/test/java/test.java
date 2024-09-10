3d91860c989c12134d0b2e5f6caf516b04dcb8f6
Hasan Küçükselek <hasankucukselek7@gmail.com>
2024-09-10 22:47:25 +0300

    first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,65 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class test {
+
+    @Test
+    void Test1(){
+        assertTrue(true);
+    }
+    @Test
+    void Test2(){
+        assertTrue(true);
+    }
+    @Test
+    void Test3(){
+        assertTrue(true);
+    }
+    @Test
+    void Test4(){
+        assertTrue(true);
+    }
+    @Test
+    void Test5(){
+        assertTrue(true);
+    }
+    @Test
+    void Test6(){
+        assertTrue(true);
+    }
+    @Test
+    void Test7(){
+        assertTrue(true);}
+    @Test
+    void Test8(){
+        assertTrue(true);
+    }
+    @Test
+    void Test9(){
+        assertTrue(true);
+    }
+    @Test
+    void Test10(){
+        assertTrue(true);
+    }
+    @Test
+    void Test11(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test12(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test13(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test14(){
+        assertTrue(true);
+    }
+}
